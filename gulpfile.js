67de01263efc7ed57eba1f06452074290ce90c25
Deshi Rahim <deshi@deshiknaves.com>
2018-04-30 23:33:06 +1000

    Formatted gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,15 @@ const rename = require('gulp-rename')
 const sourcemaps = require('gulp-sourcemaps')
 
 gulp.task('compress', () => {
-  return gulp.src('lib/bundle/*.js')
+  return gulp
+    .src('lib/bundle/*.js')
     .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(uglify())
-    .pipe(rename({
-      extname: '.min.js'
-    }))
+    .pipe(
+      rename({
+        extname: '.min.js',
+      })
+    )
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('lib/bundle'))
 })
